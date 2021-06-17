e236fa897e7c74e389bf08f204b394206e257c4f
nikita <lpenguin@yandex.ru>
2021-06-17 10:22:35 +0300

    Emotions

--- Assets/SimpleBT/Nodes/Guard.cs
+++ Assets/SimpleBT/Nodes/Guard.cs
@@ -1,42 +1,49 @@
 using System;
-using SimpleBT;
 
 namespace SimpleBT.Nodes
 {
-    public class Guard: Sequence
+    public class Guard: Node
     {
-        protected override void OnStart()
-        {
-            taskIndex = 1;
-        }
-
         protected override Status OnUpdate()
         {
             if (Children.Count < 2)
             {
-                throw new Exception($"Guard: invalid number of children ({Children.Count})");
+                throw new Exception($"Guard: invalid number of children ({Children.Count}), must be 2 exactly");
             }
 
             var condition = Children[0];
             condition.Reset();
             condition.Execute(currentContext);
+
+            var action = Children[1];
             
             switch (condition.Status)
             {
                 case Status.Running:
                     return Status.Running;
                 case Status.Failed:
-                    taskIndex = 1;
                     return Status.Failed;
                 case Status.Success:
-                    if (Children[taskIndex].Status == Status.Failed)
+                    if (action.Status == Status.Failed)
                     {
-                        for (int i = 1; i < Children.Count; i++)
-                        {
-                            Children[i].Reset();
-                        }
+                        action.Reset();
                     }
-                    return base.OnUpdate();
+                    
+                    return ExecuteAction(action);
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private Status ExecuteAction(Node action)
+        {
+            action.Execute(currentContext);
+            switch (action.Status)
+            {
+                case Status.Running:
+                case Status.Success:
+                case Status.Failed:
+                    return action.Status;
                 default:
                     throw new ArgumentOutOfRangeException();
             }
