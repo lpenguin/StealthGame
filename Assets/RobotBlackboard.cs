0ba451cb14476a13206c7cc845daca9435ad6f9b
nikita <lpenguin@yandex.ru>
2021-06-17 10:45:11 +0300

    Folder restructure

--- Assets/RobotBlackboard.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RobotBlackboard : MonoBehaviour
-{
-    public Transform target;
-    public Vector3 lastSeenPosition;
-}
