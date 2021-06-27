389cc6bf00a6e013700fad3d354b9bacca0f1dd8
nikita <lpenguin@yandex.ru>
2021-06-27 18:02:21 +0300

    SimpleBT: Until Fail node

---
+++ Assets/Scripts/SimpleBT/Nodes/UntilFail.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine.Assertions;
+
+namespace SimpleBT.Nodes
+{
+    public class UntilFail: Node
+    {
+        protected override Status OnUpdate()
+        {
+            Assert.AreEqual(Children.Count, 1, "Not can only contain 1 child");
+            var child = Children[0];
+            child.Execute(currentContext);
+            switch (child.Status)
+            {
+                case Status.Running:
+                    return Status.Running;
+                case Status.Failed:
+                    return Status.Failed;
+                case Status.Success:
+                    return Status.Running;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
