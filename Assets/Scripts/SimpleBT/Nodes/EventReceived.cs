84b699d6afa8398558047e67c34bed196b67e7a5
nikita <lpenguin@yandex.ru>
2021-06-28 16:59:59 +0300

    Event received fix

--- Assets/Scripts/SimpleBT/Nodes/EventReceived.cs
+++ Assets/Scripts/SimpleBT/Nodes/EventReceived.cs
@@ -10,36 +10,46 @@ namespace SimpleBT.Nodes
         private Parameter<object> arg2;
         private Parameter<object> arg3;
 
-        private bool _handledEvent = false;
+        // private bool _handledEvent = false;
+        private bool _registered = false;
+        private BTEvent _handledEvent = null;
 
         protected override void OnStart(){
             // TODO: deregister callback. Probably need OnAwake function
-            currentContext.EventBus.RegisterCallback(name.Value, OnNewEvent);
-            _handledEvent = false;
+            if(!_registered){
+                currentContext.EventBus.RegisterCallback(name.Value, OnNewEvent);
+                _registered = true;
+            }
+            
         }
         
         protected override Status OnUpdate()
         {
-            var result = _handledEvent ? Status.Success : Status.Failed;
-            _handledEvent = false;
-            return result;
+            if (_handledEvent != null)
+            {
+                if(_handledEvent.Arg1.HasValue){
+                    arg1.Value = _handledEvent.Arg1.Value;
+                }
+
+                if(_handledEvent.Arg2.HasValue){
+                    arg2.Value = _handledEvent.Arg2.Value;
+                }
+
+                if(_handledEvent.Arg3.HasValue){
+                    arg3.Value = _handledEvent.Arg3.Value;
+                }
+
+                _handledEvent = null;
+                return Status.Success;
+            }
+
+            return Status.Failed;
         }
 
 
 
         private void OnNewEvent(BTEvent ev){
-            _handledEvent = true;
-            if(ev.Arg1.HasValue){
-                arg1.Value = ev.Arg1.Value;
-            }
-
-            if(ev.Arg2.HasValue){
-                arg2.Value = ev.Arg2.Value;
-            }
-
-            if(ev.Arg3.HasValue){
-                arg3.Value = ev.Arg2.Value;
-            }
+            _handledEvent = ev;
         }
     }
 }
