0f504b2eddba939e52ad6159cd28ce8bc2b5ade4
nikita <lpenguin@yandex.ru>
2021-07-16 10:02:30 +0300

    Door is opening when robot is nearby

--- Assets/Scripts/GameLogic/DoorController.cs
+++ Assets/Scripts/GameLogic/DoorController.cs
@@ -11,6 +11,9 @@ namespace GameLogic
     {
         [SerializeField]
         private bool isOpened;
+
+        [SerializeField]
+        private ContactSensor3D robotSensor;
         
         private Animator _animator;
         private AudioManager _audioManager;
@@ -21,43 +24,99 @@ namespace GameLogic
             _animator = GetComponent<Animator>();
             _audioManager = GetComponent<AudioManager>();
             _animator.SetBool(On, isOpened);
+
+        }
+
+        private void OnEnable()
+        {
+            if (robotSensor != null)
+            {
+                robotSensor.onEnter.AddListener(OnRobotEnter);
+                robotSensor.onExit.AddListener(OnRobotExit);
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (robotSensor != null)
+            {
+                robotSensor.onEnter.RemoveListener(OnRobotEnter);
+                robotSensor.onExit.RemoveListener(OnRobotExit);
+            }
+        }
+        
+        private void OnRobotEnter(Collider _)
+        {
+            OpenDoor();
+        }
+        
+        private void OnRobotExit(Collider _)
+        {
+            CloseDoor();
+        }
+        
+        private bool IsRobotNearby()
+        {
+            if (robotSensor == null)
+            {
+                return false;
+            }
+
+            return robotSensor.Contacts.Count > 0;
+        }
+        
+        private void OpenDoor()
+        {
+            if (isOpened)
+            {
+                return;
+            }
+
+            isOpened = true;
+            _animator.SetBool(On, true);
+            _audioManager.PlayAudio("False");
+        }
+
+        private void CloseDoor()
+        {
+            if (!isOpened)
+            {
+                return;
+            }
+
+
+            if (IsRobotNearby())
+            {
+                return;
+            }
+            isOpened = false;
+            _animator.SetBool(On, false);
+            _audioManager.PlayAudio("Close");
         }
         
         public void HandleSignal(SignalType signal)
         {
-            bool changed;
             switch (signal)
             {
                 case SignalType.On:
-                    changed = !isOpened;
-                    isOpened = true;
+                    OpenDoor();
                     break;
                 case SignalType.Off:
-                    changed = isOpened;
-                    isOpened = false;
+                    CloseDoor();
                     break;
                 case SignalType.Toggle:
-                    changed = true;
-                    isOpened = !isOpened;
+                    if (isOpened)
+                    {
+                        CloseDoor();
+                    }
+                    else
+                    {
+                        OpenDoor();
+                    }
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(signal), signal, null);
             }
-            
-            _animator.SetBool(On, isOpened);
-            
-            if (changed)
-            {
-                if (isOpened)
-                {
-                    _audioManager.PlayAudio("Open");
-                }
-                else
-                {
-                    _audioManager.PlayAudio("Close");
-                }
-            }
-            
         }
     }
 }
